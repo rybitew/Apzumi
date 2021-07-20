53e83bf0dba452dded711d26d10668f7b7602cd9
rybitew <rybitew@gmail.com>
2021-07-21 01:35:52 +0200

    add controller error handling

--- src/main/java/com/example/task/post/PostController.java
+++ src/main/java/com/example/task/post/PostController.java
@@ -1,11 +1,14 @@
 package com.example.task.post;
 
+import com.example.task.exception.InvalidParamException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping("posts")
@@ -20,27 +23,53 @@ public class PostController {
 
     @GetMapping(value = "/all")
     public ResponseEntity<List<PostDto>> getAllPosts() {
-        return ResponseEntity.ok(postService.getAll());
+        try {
+            return ResponseEntity.ok(postService.getAll());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/get")
     public ResponseEntity<List<PostDto>> getFilteredPosts(@RequestParam("title") String title) {
-        return ResponseEntity.ok(postService.getByTitle(title));
+        try {
+            return ResponseEntity.ok(postService.getByTitle(title));
+        } catch (InvalidParamException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PutMapping("/edit")
     public ResponseEntity<PostDto> editPost(@RequestBody PostDto data) {
-        return ResponseEntity.ok(postService.editPost(data));
+        try {
+            return ResponseEntity.ok(postService.editPost(data));
+        } catch (InvalidParamException | NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PutMapping("/update")
     public ResponseEntity<Integer> updatePosts() {
-        return ResponseEntity.ok(postService.updatePosts());
+        try {
+            return ResponseEntity.ok(postService.updatePosts());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable Integer id) {
-        postService.deletePost(id);
-        return ResponseEntity.ok().build();
+        try {
+            postService.deletePost(id);
+            return ResponseEntity.ok().build();
+        } catch (InvalidParamException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
